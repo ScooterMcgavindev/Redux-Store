4bbb43cb399c3b4a79e4c44dbb1b93c75c10b0b4
ScooterMcgavin21 <scott.skrobel@gmail.com>
2022-05-04 18:22:19 -0600

    connect Market to the redux store

---
+++ client/components/Market.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Market(props) {
+  return (
+    <div className='marketBox'>
+      <p>MarketId : {props.marketId}</p>
+      <p>Location : {props.location}</p>
+      <p>Cards : {props.cards}</p>
+      <p>% of total : {}</p>
+      <div>
+        {/* add card will update the "Cards" total. onClick will dispatch this action. */}
+        {/* Delete cards will decrement the "Cards" total */}
+        <button>Add Card</button> <button>Delete Card</button>
+      </div>
+    </div>
+  );
+}
+
+/** Connect function to connect component to the store */
+const mapStateToProps = state => ({
+  marketID: state.markets.lastMarketID,
+  location: state.markets.newLocation,
+  cards: state.markets.totalCards,
+});
+
+const mapDispatchToProps = dispatch => ({
+  //dispatch add card and delete card actions here
+  // addCardActionCreator: marketID => dispatch(deleteCardActionCreator(marketID)),
+  // deleteCardActionCreator: totalCards => dispatch(addReminder(totalCards)),
+});
+
+export default connect(mapStateToProps, null)(Market);
--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,1 +1,9 @@
 /** Renders MarketContainer and the TotalDisplay */
+
+import React from 'react';
+
+function MainContainer() {
+  return <div>MainContainer</div>;
+}
+
+export default MainContainer;
